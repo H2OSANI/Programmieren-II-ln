06dbb2e892e822e188519d65c36720bcda06e2be
H2OSANI <sandro.podo@gmx.de>
2021-11-27 11:28:42 +0100

    test Method

--- app/src/main/java/com/example/programmiereniiln/MainActivity.java
+++ app/src/main/java/com/example/programmiereniiln/MainActivity.java
@@ -17,6 +17,7 @@ import com.example.programmiereniiln.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -72,5 +73,8 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+    public void test() {
+        System.out.println("test");
     }
 }
